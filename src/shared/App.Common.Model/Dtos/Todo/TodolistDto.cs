0c2276307f7bddcb53c2fd8aa73219c707771e00
aafgani <andrya.afgani@gmail.com>
2025-07-30 10:12:26 +0700

    refactor: :sparkles: todos fresh new design using component

--- src/shared/App.Common.Model/Dtos/Todo/TodolistDto.cs
+++ src/shared/App.Common.Model/Dtos/Todo/TodolistDto.cs
@@ -7,5 +7,6 @@
         public string Description { get; set; }
         public DateTime CreatedAt { get; set; }
         public string UserId { get; set; }
+        public bool IsCompleted { get; set; }
     }
 }
