6963323fd426247983241f27f2114f03b6e86a1d
aafgani <andrya.afgani@gmail.com>
2025-08-05 15:39:01 +0700

    refactor: :lock: use TodoApiClient

--- src/apps/App.Web.Client/Utilities/CustomViewComponent/Todos/TodoListViewComponent.cs
+++ src/apps/App.Web.Client/Utilities/CustomViewComponent/Todos/TodoListViewComponent.cs
@@ -1,25 +1,20 @@
-using App.Common.Domain.Dtos.Todo;
-using App.Common.Domain.Pagination;
+using App.Web.Client.Utilities.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace App.Web.Client.Utilities.CustomViewComponent.Todos;
 
 public class TodolistViewComponent : ViewComponent
 {
-    private readonly IHttpClientFactory _httpClientFactory;
-    private readonly string _apiBaseUrl;
+    private readonly ITodoApiClient _todoApiClient;
 
-    public TodolistViewComponent(IHttpClientFactory httpClientFactory, IConfiguration configuration)
+    public TodolistViewComponent(ITodoApiClient todoApiClient)
     {
-        _httpClientFactory = httpClientFactory;
-        _apiBaseUrl = configuration["TodoApiBaseUrl"] ?? "http://localhost:8081";
+        _todoApiClient = todoApiClient;
     }
 
     public async Task<IViewComponentResult> InvokeAsync()
     {
-        var client = _httpClientFactory.CreateClient();
-        var pagedResult = await client.GetFromJsonAsync<PagedResult<TodolistDto>>(_apiBaseUrl + "/todos?Page=1&PageSize=5");
-
+        var pagedResult = await _todoApiClient.GetTodosAsync(1, 5);
         return View(pagedResult);
     }
 }
