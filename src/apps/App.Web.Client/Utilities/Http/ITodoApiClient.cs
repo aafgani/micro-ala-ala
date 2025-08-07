871985d5a50d0c955a4dc85a76f8023b68530eed
aafgani <andrya.afgani@gmail.com>
2025-08-07 11:05:13 +0700

    feat: Added todos stats ComponentBox

--- src/apps/App.Web.Client/Utilities/Http/ITodoApiClient.cs
+++ src/apps/App.Web.Client/Utilities/Http/ITodoApiClient.cs
@@ -6,4 +6,5 @@ namespace App.Web.Client.Utilities.Http;
 public interface ITodoApiClient
 {
     Task<PagedResult<TodolistDto>> GetTodosAsync(int pageNumber = 1, int pageSize = 10, CancellationToken cancellationToken = default);
+    Task<TodoStatsDto> GetTodoStatsAsync(string userId = null, CancellationToken cancellationToken = default);
 }
