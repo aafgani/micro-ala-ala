1049d4495c736c3e61910b809fa20a48392f1033
Andrya Afgani <andrya.afgani@gmail.com>
2025-08-21 14:38:59 +0700

    style: :recycle: remove web client as no longer needed (#59)

--- src/apps/App.Web.Client/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace App.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
